3983ace65ffeed6b08d9a37692f83eed8d03c586
Justin <jvcruz@ucdavis.edu>
2018-03-20 00:15:49 -0700

    home route created

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App'
+import Home from './Home';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', component: Home}
+];
+
+const router = new VueRouter({
+	routes,
+	mode: 'history'
+});
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
