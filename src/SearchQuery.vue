cc8e4536f77ec0bf28d9d2006eb72f9d32482973
Justin <jvcruz@ucdavis.edu>
2018-03-20 03:06:57 -0700

    search results page made for handling wikipedia api

---
+++ src/SearchQuery.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ query }}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     query: this.$route.params.query 
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
