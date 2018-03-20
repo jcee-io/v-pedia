2c2894992903790fa5efcd0adb6cfff3eb79f2de
Justin <jvcruz@ucdavis.edu>
2018-03-20 03:42:22 -0700

    articles now render

--- src/SearchQuery.vue
+++ src/SearchQuery.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>{{ query }}</h1>
+  <div>
+    <h1>{{ query }}</h1>
+    <div v-for="article in articles">
+      <h2>Title:  {{ article.title }}</h2>
+      <p>Description: {{ article.snippet }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-     query: this.$route.params.query 
+     query: this.$route.params.query,
+     cors: 'https://cors-anywhere.herokuapp.com/',
+     url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=',
+     articles: []
     }
+  },
+  created() {
+    this.$http.get(this.cors + this.url + this.query)
+      .then(function(data) {
+        console.log(data.body.query.search);
+        this.articles = data.body.query.search;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   }
 }
 </script>
