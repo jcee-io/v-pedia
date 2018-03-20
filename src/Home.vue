a12a97e3cfd577abf7a47225b36c23934e3fb38b
Justin <jvcruz@ucdavis.edu>
2018-03-20 02:55:25 -0700

    formatted search bar and buttons with css, and removed body css

--- src/Home.vue
+++ src/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <h1>Wikipedia Article Selector</h1>
-    <input v-model="query">
+    <div class="container">
+      <input id="search-bar" class="form-control" v-model="query">
+      <div id="search-buttons">
+        <button class="btn btn-outline-light btn-lg">Wikipedia Search</button>
+        <button class="btn btn-outline-light btn-lg">I'm feeling Random</button>   
+      </div>
+    </div>
     <h2>{{ query }}</h2>
   </div>
 </template>
@@ -18,10 +24,17 @@ export default {
 }
 </script>
 
-<style>
-  body {
-    background: black;
-    color: whitesmoke;
-    text-align: center;
+<style scoped>
+  #search-bar {
+    margin-top: 35vh;
+  }
+  #search-buttons {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  #search-buttons button {
+    width: 30%;
   }
 </style>
