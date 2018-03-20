0bf40dfc942fd991067571bd2b6e58c3e2d43966
Justin <jvcruz@ucdavis.edu>
2018-03-19 23:52:39 -0700

    added vue-resource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
+Vue.use(VueResource);
 Vue.use(VueRouter);
 
 new Vue({
